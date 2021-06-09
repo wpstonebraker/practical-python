3e79b346024ce8767a501a0ac2b7d2c8941c7a3d
Paul Stonebraker <wpstonebraker@gmail.com>
2021-06-09 15:23:39 -0400

    work on report.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,46 @@
 # pcost.py
 #
 # Exercise 1.27
+
+# def portfolio_cost(filename):
+#     total = 0
+#     file = open(filename, 'rt')
+#     headers = next(file).split(',')
+
+#     for line in file:
+#         row = line.split(',')
+#         # numberofstocks = float(row[1])
+#         # priceofstock = float(row[2])
+#         # sumofstock = numberofstocks * priceofstock
+#         # total = total + sumofstock
+#         total += float(row[1]) * float(row[2])
+#     return total
+
+# cost = portfolio_cost('Data/portfolio.csv')
+# print('Total cost', cost)
+
+import csv
+def portfolio_cost(filename):
+    total_cost = 0
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            try:
+                nshares = int(row[1])
+                price = float(row[2])
+                total_cost += nshares * price
+            except ValueError:
+                print('Bad row:', row)
+        
+    return total_cost
+
+import sys
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = input('Enter a filename:')
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,49 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+# def portfolio_cost(filename):
+
+#     total_cost = 0
+
+#     with open(filename, 'rt') as f:
+#         rows = csv.reader(f)
+#         headers = next(rows)
+#         for row in rows:
+#             nshares = int(row[1])
+#             price = float(row[2])
+#             total_cost += nshares * price
+#     return total_cost
+
+def read_portfolio(filename):
+    # portfolio = []
+
+    # with open(filename, 'rt') as file:
+    #     rows = csv.reader(file)
+    #     headers = next(rows)
+    #     for row in rows:
+    #         holding = row[0], int(row[1]), float(row[2])
+    #         portfolio.append(holding)
+    #     # for name, shares, price in rows:
+    #     #     print(name, shares, price)
+    
+    # return portfolio
+
+    portfolio = []
+
+    with open(filename, 'rt') as file:
+        rows = csv.reader(file)
+        headers = next(rows)
+
+        for row in rows:
+            stock = {}
+            name, shares, price = row
+            stock['name'] = name
+            stock['shares'] = int(shares)
+            stock['price'] = float(price)
+            portfolio.append(stock)
+
+    return portfolio
+
