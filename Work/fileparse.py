5a0ccb90025e4dc9f5e68811beed4daa1c55d62c
Paul Stonebraker <wpstonebraker@gmail.com>
2021-06-09 21:17:16 -0400

    start on fileparse

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,23 @@
 # fileparse.py
 #
 # Exercise 3.3
+
+import csv
+
+def parse_csv(filename):
+    '''
+    Parse a CSV file into a list of records
+    '''
+    with open(filename) as file:
+        rows = csv.reader(f)
+
+        # Read the file headers
+        headers = next(rows)
+        records = []
+        for row in rows:
+            if not row: # skips row with no data
+                continue
+            record = dict(zip(headers, row))
+            records.append(record)
+
+    return records
