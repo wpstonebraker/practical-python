8a7b875c60d1534a335b37c761506f7c9d2e73cd
Paul Stonebraker <wpstonebraker@gmail.com>
2021-06-09 21:26:32 -0400

    finish 3.4

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,19 +4,29 @@
 
 import csv
 
-def parse_csv(filename):
+def parse_csv(filename, select=None):
     '''
     Parse a CSV file into a list of records
     '''
     with open(filename) as file:
-        rows = csv.reader(f)
+        rows = csv.reader(file)
 
         # Read the file headers
         headers = next(rows)
+
+        if select:
+            indices = [headers.index(colname) for colname in select] # search for index of colname in headers
+            headers = select
+        else:
+            indices = [] # if blank it'll skip conditional in loop below
+
+        
         records = []
         for row in rows:
             if not row: # skips row with no data
                 continue
+            if indices: # if indices isn't blank, replace row with new row of relevant data
+                row = [row[index] for index in indices]
             record = dict(zip(headers, row))
             records.append(record)
 
